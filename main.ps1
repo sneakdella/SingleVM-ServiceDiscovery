913e9df0c3faed67c9871fb9a46a38bfb4bda911
TEST <TEST@sillybusses.net>
2023-04-28 23:17:53 -0400

    Added Get-vROpsAccessToken

---
+++ main.ps1
@@ -0,0 +1,51 @@
+######## HEADER FOR API CALLS ##############
+$global:Headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$global:Headers.Add("Content-Type", "application/json")
+$global:Headers.Add("Accept", "application/json")
+$global:accesstoken = ''
+######## END HEADER FOR API CALLS ##########
+
+$RemoteCollector = "10.0.0.27"
+$Credential = Get-Credential -Message "Please provide your vROps Credentials"
+$AuthSource = Read-Host "Enter the auth source of the account. [Enter LOCAL if it's just local]: "
+
+if ($AuthSource -eq "") {
+    $AuthSource = "LOCAL"
+}
+
+function Get-vROpsAccessToken {
+    param (
+        [Parameter(Mandatory=$true)]$RemoteCollector,
+        [Parameter(Mandatory=$true)][System.Management.Automation.PSCredential]$Credential,
+        [Parameter(Mandatory=$false)]$AuthSource="LOCAL",
+        [Parameter(Mandatory=$false)]$Refresh=$false,
+        [Parameter(Mandatory=$false)]$FunctionDebug=$false
+    )
+
+    #Write-Host $Credential.GetNetworkCredential().Password
+
+    $jsonBody = @{
+        "username"=$Credential.UserName;
+        "authSource"=$AuthSource;
+        "password"=$Credential.GetNetworkCredential().Password;
+    } | ConvertTo-JSON
+    
+    If ($Refresh -eq $false) {
+        $global:accesstoken = Invoke-RestMethod "https://$RemoteCollector/suite-api/api/auth/token/acquire?_no_links=true" -Method 'POST' -Headers $Headers -Body $jsonBody -SkipCertificateCheck
+    } elseif ($Refresh -eq $true) {
+        $global:Headers.Remove("Authorization")
+        $global:accesstoken = Invoke-RestMethod "https://$RemoteCollector/suite-api/api/auth/token/acquire?_no_links=true" -Method 'POST' -Headers $Headers -Body $jsonBody -SkipCertificateCheck
+    }
+
+    # Clear authentication from memory
+    $jsonBody = ""
+    
+    # DEBUG: Show $accesstoken
+    If ($FunctionDebug -eq $true){
+        Write-Host "function Get-vROpsAccessToken | `$accesstoken DEBUG:" $accesstoken
+    }
+    
+    $global:Headers.Add("Authorization", "vRealizeOpsToken " +$global:accesstoken.token)
+}
+
+Get-vROpsAccessToken -RemoteCollector $RemoteCollector -Credential $Credential -AuthSource $AuthSource -FunctionDebug $true
